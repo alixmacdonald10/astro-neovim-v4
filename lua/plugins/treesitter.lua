b823950518cb694ec5058ad963fb9c28d59b6212
Alix Macdonald <amacdonald@bscglobal.com>
2024-08-19 12:38:07 +0100

    added hurl.nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
+      "hurl",
     })
   end,
 }
